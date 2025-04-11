0ebd3c3f74cf7aa72cd54cec61ae96f639ddc8e1
Indianasol <ideirmendjian@mail.austral.edu.ar>
2025-04-11 11:34:10 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,6 +2,6 @@ def slice_simple():
     texto = "Awesome"
   
     print(texto[0:3].lower())
-    print(texto[0:5].lower())
+    print(texto[2:5].lower())
     print(texto[0:4].lower() + texto[4:7].lower())
 
